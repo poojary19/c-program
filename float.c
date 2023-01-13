a2e49c50115d283902f3f22bc2f0a88a5edefab5
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-01-13 21:40:46 +0530

    Add files via upload

---
+++ float.c
@@ -0,0 +1,11 @@
+v#include <stdio.h>
+#include <math.h>
+int main()
+{
+   float num, root;
+   printf("Enter a number: ");
+   scanf("%f", &num);
+   root = sqrt(num);
+   printf("Square root of %.2f = %.2f", num, root);
+   return 0;
+}
